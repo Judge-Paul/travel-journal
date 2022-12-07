73c8e7deffc6781c36937f25889a184d94d2563e
Judge-Paul <judgepaulogebe3@gmail.com>
2022-12-07 06:22:57 +0100

    feat: pass values as props

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div className="container card-element">
             <div className="row">
@@ -24,19 +24,19 @@ export default function Card() {
                     >
                         <path d="M213.285 0h-.608C139.114 0 79.268 59.826 79.268 133.361c0 48.202 21.952 111.817 65.246 189.081 32.098 57.281 64.646 101.152 64.972 101.588a4.8 4.8 0 003.847 1.934c.043 0 .087 0 .13-.002a4.805 4.805 0 003.868-2.143c.321-.486 32.637-49.287 64.517-108.976 43.03-80.563 64.848-141.624 64.848-181.482C346.693 59.825 286.846 0 213.285 0zm61.58 136.62c0 34.124-27.761 61.884-61.885 61.884-34.123 0-61.884-27.761-61.884-61.884s27.761-61.884 61.884-61.884c34.124 0 61.885 27.761 61.885 61.884z"></path>
                     </svg>
-                        <span className="card--country pl-2">JAPAN</span>
+                        <span className="card--country pl-2">{props.country}</span>
                         <span className="pl-4">
                             <a href="#" className="card--maplink" >View on Google Maps</a>
                         </span>
                     </div>
                     <div className="card--title mb-4">
-                        Mount Fuji
+                        {props.title}
                     </div>
                     <div className="card--date mt-3 mb-3">
-                        <span>12 Jan, 2021</span> - <span>24 Jan, 2021</span>
+                        <span>{props.startDate}</span> - <span>{props.endDate}</span>
                     </div>
                     <div className="card--description">
-                        Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                        {props.description}
                     </div>
                 </div>
             </div>
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Card from './Card'
+import Data from './data.js'
 
 export default function Cards() {
     return (
         <div className= "cards">
-            <Card />
+            <Card
+            country="JAPAN"
+            title="Mount Fuji"
+            startDate="12 Jan, 2021"
+            endDate="24 Jan, 2021"
+            description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+            />
         </div>
     )
 }
