ba96a98b39495f2c19b0da121523dc8d78d22600
Judge-Paul <judgepaulogebe3@gmail.com>
2022-12-07 07:03:34 +0100

    feat: card element to get properties using data.js file

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 export default function Card(props) {
+
     return (
         <div className="container card-element">
             <div className="row">
                 <div className="col-lg-3">
-                    <img src="https://source.unsplash.com/WLxQvbMyfas" />
+                    <img src={props.item.imageUrl} />
                 </div>
                 <div className="col-lg-9">
                     <div className='pt-4 pb-2'>
@@ -24,19 +25,19 @@ export default function Card(props) {
                     >
                         <path d="M213.285 0h-.608C139.114 0 79.268 59.826 79.268 133.361c0 48.202 21.952 111.817 65.246 189.081 32.098 57.281 64.646 101.152 64.972 101.588a4.8 4.8 0 003.847 1.934c.043 0 .087 0 .13-.002a4.805 4.805 0 003.868-2.143c.321-.486 32.637-49.287 64.517-108.976 43.03-80.563 64.848-141.624 64.848-181.482C346.693 59.825 286.846 0 213.285 0zm61.58 136.62c0 34.124-27.761 61.884-61.885 61.884-34.123 0-61.884-27.761-61.884-61.884s27.761-61.884 61.884-61.884c34.124 0 61.885 27.761 61.885 61.884z"></path>
                     </svg>
-                        <span className="card--country pl-2">{props.country}</span>
+                        <span className="card--country pl-2">{(props.item.location).toUpperCase()}</span>
                         <span className="pl-4">
-                            <a href="#" className="card--maplink" >View on Google Maps</a>
+                            <a href={props.item.googleMapsUrl} className="card--maplink" >View on Google Maps</a>
                         </span>
                     </div>
                     <div className="card--title mb-4">
-                        {props.title}
+                        {props.item.title}
                     </div>
                     <div className="card--date mt-3 mb-3">
-                        <span>{props.startDate}</span> - <span>{props.endDate}</span>
+                        <span>{props.item.startDate}</span> - <span>{props.item.endDate}</span>
                     </div>
                     <div className="card--description">
-                        {props.description}
+                        {props.item.description}
                     </div>
                 </div>
             </div>
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -3,15 +3,17 @@ import Card from './Card'
 import Data from './data.js'
 
 export default function Cards() {
+    const card = Data.map( function(Data) {
+        return (
+            <Card
+            key={Data.id}
+            item={Data}
+            />
+        )
+    })
     return (
         <div className= "cards">
-            <Card
-            country="JAPAN"
-            title="Mount Fuji"
-            startDate="12 Jan, 2021"
-            endDate="24 Jan, 2021"
-            description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-            />
+            {card}
         </div>
     )
 }
